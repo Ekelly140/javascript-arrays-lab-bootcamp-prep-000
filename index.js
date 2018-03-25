6936ddc819c80bc8a511991ce4f0004f58e1a63a
ERIC KELLY <eakelly220@gmail.com>
2018-03-25 03:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ function destructivelyRemoveFirstKitten(){
   return kittens;
 }
 function appendKitten(name){
-  kittens = [...names, kittens]
+  kittens = [...name, kittens]
   return kittens;
 }
